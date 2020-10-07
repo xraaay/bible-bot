5c017096814eeebe2742c08151b35c8de84ddd57
klxxsb <kimborres@hotmail.com>
2020-10-07 02:12:13 -0400

    me command menu

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+
 module.exports = {
 	name: 'help',
 	description: 'Gives a list of commands or information about a command',
@@ -5,26 +7,26 @@ module.exports = {
 	usage: '[command name]',
 	args: false,
 	execute(message, args) {
-		const prefix = process.env.PREFIX;
-		const commands = message.client.commands;
-		const commandList = commands.map(command => '**' + process.env.PREFIX + command.name + '**').join(', ');
-		if (!args.length) {
-			message.reply(`here's a list of my commands: ${commandList}, try ${process.env.PREFIX}help [command name] for more info`);
-		}
-		else {
-			const name = args[0].toLowerCase();
-			const data = [];
-			const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
-			if (!command) {
-				return message.reply('that\'s not a valid command');
-			}
+	
 
-			if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-			if (command.description) data.push(`**Description:** ${command.description}`);
-			if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+		const exampleEmbed = new Discord.MessageEmbed()
+			.setColor('#0099ff')
+			.setTitle('The Pope')
+			.setURL('https://discord.js.org/')
+			// .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+			.setDescription('Command List')
+			.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+			.addFields(
 
-			message.channel.send(data, { split: true });
-		}
+				{ name: 'b!reminder', value: 'Set a reminder'},
+				{ name: 'b!hungry', value: 'Decide what to eat through RNG'},
+				{ name: 'b!8Ball', value: 'Ask a \'yes\' or \'no\' question'},
+				{ name: 'b!poll', value: 'Create a poll with reactions to vote'},
+			)
+			.setTimestamp()
+			.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+		message.reply(exampleEmbed)
 	},
 };
 
