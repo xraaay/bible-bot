5266b33d0fc522b7fc563ee0a85dfcb8fa06e7b7
Raymond Xie <raymond.y.xie@gmail.com>
2020-10-14 14:17:18 -0700

    add embed

--- commands/eat.js
+++ commands/eat.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const Discord = require('discord.js');
 const yelp_token = process.env.YELP_TOKEN;
 const yelp = require('yelp-fusion');
 const client = yelp.client(yelp_token);
@@ -15,16 +16,31 @@ module.exports = {
 		// console.log(yelp_token);
 		// message.reply();
 		client.search({
-			term: 'tacos',
-			location: 'new york',
+			term: args[0],
+			location: args[1],
 		})
 			.then(response => {
+				console.log(args);
 				// console.log(response.jsonBody);
-				const arr = [];
-				for(let i = 0; i < 10; i++) {
-					arr.push(response.jsonBody.businesses[i].name);
-				}
-				message.reply(JSON.stringify(arr));
+				// const arr = [];
+				// for (let i = 0; i < 10; i++) {
+				// 	arr.push(response.jsonBody.businesses[i].name);
+				// }
+				const business = response.jsonBody.businesses[0];
+				const embed = new Discord.MessageEmbed()
+					.setColor('#0099ff')
+					.setTitle(business.name)
+					.setURL(business.url)
+					.addFields(
+						{ name: 'Categories', value: business.categories.map(item => item.title) },
+						{ name: 'Price', value: business.price },
+						{ name: 'Address', value: `${business.location.display_address[0]}, ${business.location.display_address[1]}` },
+						{ name: 'Phone', value: business.display_phone, inline: true },
+						{ name: 'Rating', value: business.rating },
+					)
+					.setImage(business.image_url)
+					.setTimestamp();
+				message.reply(embed);
 			})
 			.catch(e => {
 				console.log(e);
